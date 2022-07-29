521e9695e9b527690e6a66391815d9c9c39bc4a6
unknown <bazo20061996@gmail.com>
2022-07-29 14:41:20 +0700

    MD2-bai5-BT modifier

---
+++ src/Criscle.java
@@ -0,0 +1,33 @@
+public class Criscle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Criscle() {
+
+    }
+
+    public Criscle(double radius,String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea(){
+        return Math.PI * radius*radius;
+    }
+    public double setArea(){
+        double area = Math.PI * radius*radius;
+        return area;
+    }
+    public String display(){
+        return this.radius;
+        return this.color;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Criscle tamGiac1 = new Criscle(3,"blue");
+        System.out.println("thong so tam giac: " + tamGiac1.display());
+
+    }
+}
